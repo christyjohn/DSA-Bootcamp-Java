a9ca7093bd4042cb37c9e386c72fe541550073c8
Christy John <christyjohn.crz@gmail.com>
2026-08-15 16:49:05 +0530

    Stable Bubble sort with O(n) best case and O(n^2) worst case.

---
+++ src/io/github/christyjohn/p03_sorting/non_recursive/BubbleSortExample.java
@@ -0,0 +1,36 @@
+package io.github.christyjohn.p03_sorting.non_recursive;
+
+import java.util.Arrays;
+
+public class BubbleSortExample {
+    public static void main(String[] args) {
+        int[] nums = { 3, 56, 2, 78, 109, 23, -4, 5};
+        System.out.println(Arrays.toString(bubbleSort(nums)));
+
+        nums = new int[]{ -3, -56, -2, -78, -109, -23, 4, -5};
+        System.out.println(Arrays.toString(bubbleSort(nums)));
+    }
+
+    public static int[] bubbleSort(int[] nums) {
+
+        for(int i = 0; i < nums.length; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < nums.length - (i + 1); j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int swap = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = swap;
+                    swapped = true;
+                }
+            }
+
+            // If no two elements were swapped by the inner loop, the array is sorted!
+            if (!swapped) {
+                break;
+            }
+        }
+
+        return nums;
+    }
+}
